a4e7c32e00e767d0b19bcbb90db4fb39c98998e1
Paul Todd <paulxtodd@gmail.com>
2026-03-13 19:12:13 +0000

    vite configured

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 document.getElementById("helloBtn").addEventListener("click", () => {
   document.getElementById("output").textContent =
     "Hello from your Firebase-hosted iPad app.";
